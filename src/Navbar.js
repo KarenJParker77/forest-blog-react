ff739eaafdb8d34f77c307422cf1222a20629787
KarenJParker77 <karen.mccrackle@gmail.com>
2023-03-27 18:06:53 +0100

    final

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <img src="forest.png" alt="forest crest" className="crest" />
+      <img src="NF-stars.png" alt="forest crest" className="crest" />
       <h1>The Nottingham Forest blog</h1>
       <div className="links">
         <Link to="/">Home</Link>
